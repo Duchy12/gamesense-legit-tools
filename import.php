656b5ad41f214bf6c7e3be00ffff7ebc78556bc6
Duchy <danielbabcan@gmail.com>
2023-04-01 13:21:33 +0200

    Fixed wrong url in import statement

--- import.php
+++ import.php
@@ -2,5 +2,5 @@
     "name": "Wingman",
     "description":"Wingman nade data by @Duchy",
     "update_timestamp": 1600000000,
-    "url_format": "https://raw.githubusercontent.com/Duchy12/nade-locs/locations/%map%.json",
+    "url_format": "https://raw.githubusercontent.com/Duchy12/nade-locs/main/locations/%map%.json",
 }
