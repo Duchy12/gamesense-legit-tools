d92f5fe8fec2520258b617d5d32884c0ee50a6f4
Duchy <danielbabcan@gmail.com>
2023-04-01 18:40:19 +0200

    Updated import.php

--- import.php
+++ import.php
@@ -1,5 +1,5 @@
 {
-    "name": "Wingman",
+    "name": "Legit Nade Locations",
     "description":"Wingman nade data by @Duchy",
     "update_timestamp": 1600000000,
     "url_format": "https://raw.githubusercontent.com/Duchy12/nade-locs/main/locations/%map%.json",
